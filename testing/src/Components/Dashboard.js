6c252095090919574d09164a8a99aebcac32e4fb
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-31 12:22:14 -0700

    Added Material-UI and buttons to Dashboard

---
+++ testing/src/Components/Dashboard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Button, ButtonGroup } from "@material-ui/core";
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard-container">
+      <ButtonGroup size="large" color="secondary" fullWidth>
+        <Button variant="contained">Strike</Button>
+        <Button variant="contained">Ball</Button>
+        <Button variant="contained">Foul</Button>
+        <Button variant="contained">Hit</Button>
+      </ButtonGroup>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ testing/src/Components/Display.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Dashboard from "./Dashboard";
+
+const Display = () => {
+  return (
+    <>
+      <h1>From Display</h1>
+      <Dashboard />
+    </>
+  );
+};
+
+export default Display;
